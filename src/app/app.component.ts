cb4010969436f84ea812414796c3c8d294871e02
ttmmnn <124642000+ttmmnn@users.noreply.github.com>
2023-09-24 22:33:31 +0900

    create_new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `<h1>Hello World!</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-first-app';
+  title = 'homes';
 }
